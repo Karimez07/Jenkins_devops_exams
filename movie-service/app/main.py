86fd699f1c8c18cb0905773c652708c8bd1b60d3
Sofian <94293756+Karimez07@users.noreply.github.com>
2026-06-04 11:35:42 +0200

    Update main.py

--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -14,5 +14,9 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+@app.get("/api/v1/checkapi")
+async def check_api():
+    return {"status": "ok"}
+
 
 app.include_router(movies, prefix='/api/v1/movies', tags=['movies'])
