1ad23bb4cd90c06dcfcc3b140684bd257041e90b
Sofian Ez-ahery <top10play@gmail.com>
2026-06-07 11:09:22 +0200

    Ok

--- movie-service/app/api/service.py
+++ movie-service/app/api/service.py
@@ -4,6 +4,7 @@ import httpx
 CAST_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/casts/'
 
 def is_cast_present(cast_id: int):
-    url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
-    r = httpx.get(f'{url}{cast_id}')
+    base_url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
+    cast_url = f"{base_url.rstrip('/')}/{cast_id}/"
+    r = httpx.get(cast_url)
     return True if r.status_code == 200 else False
